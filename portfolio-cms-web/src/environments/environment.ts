61b633c44ee253215c55c9a569f3e300b1996506
Pitambar Jha <pitambarzha@gmail.com>
2025-11-13 14:22:15 +0000

    update docker and environment to ensure flow

--- portfolio-cms-web/src/environments/environment.ts
+++ portfolio-cms-web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'http://localhost:8085/api',
   tenantId: '' // Will be set dynamically or from localStorage
 };
